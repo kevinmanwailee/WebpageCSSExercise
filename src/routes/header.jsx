32d55f156f2abebb85afd05877ee0fac19ff19bb
kevinmanwailee <kevin.manwai.lee@gmail.com>
2024-07-09 16:24:54 -0600

    Populated sidebar, fixed resizing issues with header

--- src/routes/header.jsx
+++ src/routes/header.jsx
@@ -1,44 +1,53 @@
-import * as React from 'react';
-import { ThemeProvider, createTheme } from '@mui/material/styles';
-import { CssBaseline, Paper, InputBase, IconButton,
-  Button, Box, Drawer, List, Divider, ListItem, ListItemButton,
-  ListItemText } from '@mui/material';
-import HomeIcon from '@mui/icons-material/Home';
-  import MenuIcon from '@mui/icons-material/Menu';
-  import SearchIcon from '@mui/icons-material/Search';
-  import MicIcon from'@mui/icons-material/Mic';
-  import MoreVertIcon from '@mui/icons-material/MoreVert';
-  import PersonOutlineIcon from '@mui/icons-material/PersonOutline';
-  import {Link} from 'react-router-dom';
-  import "../Header.css";
+import * as React from "react";
 
-  const darkTheme = createTheme({
-  palette: {
-    mode: 'dark',
-  },
-});
+import {
+  Paper,
+  InputBase,
+  IconButton,
+  Button,
+  Box,
+  Drawer,
+  List,
+  Divider,
+  ListItem,
+  ListItemButton,
+  ListItemText,
+  ListItemIcon,
+} from "@mui/material";
+import HomeIcon from "@mui/icons-material/Home";
+import MenuIcon from "@mui/icons-material/Menu";
+import SearchIcon from "@mui/icons-material/Search";
+import MicIcon from "@mui/icons-material/Mic";
+import SlideshowIcon from "@mui/icons-material/Slideshow";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
+import SubscriptionsIcon from "@mui/icons-material/Subscriptions";
+import VideoLibraryIcon from '@mui/icons-material/VideoLibrary';
+import PersonOutlineIcon from "@mui/icons-material/PersonOutline";
+import SettingsIcon from '@mui/icons-material/Settings';
+import HistoryIcon from '@mui/icons-material/History';
+import { Link } from "react-router-dom";
+import "../Header.css";
 
 function Searchbar() {
   return (
     <Paper
       component="form"
-      sx={{ p: '2px 4px', display: 'flex', alignItems: 'center', width: 500 }}
-      style={{ borderRadius:"32px" }}
+      sx={{ p: "2px 4px", display: "flex", alignItems: "center", width: 500 }}
+      style={{ borderRadius: "32px" }}
     >
       <InputBase
         sx={{ ml: 1, flex: 1 }}
         placeholder="Search..."
-        inputProps={{ 'aria-label': 'search google maps' }}
+        inputProps={{ "aria-label": "search google maps" }}
       />
       <Divider sx={{ height: 28, m: 0.5 }} orientation="vertical" />
 
-      <IconButton type="button" sx={{ p: '10px' }} aria-label="search">
+      <IconButton type="button" sx={{ p: "10px" }} aria-label="search">
         <SearchIcon />
       </IconButton>
     </Paper>
   );
 }
-
 
 export default function Header() {
   const [state, setState] = React.useState({
@@ -46,7 +55,10 @@ export default function Header() {
   });
 
   const toggleDrawer = (anchor, open) => (event) => {
-    if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
+    if (
+      event.type === "keydown" &&
+      (event.key === "Tab" || event.key === "Shift")
+    ) {
       return;
     }
 
@@ -54,62 +66,177 @@ export default function Header() {
   };
 
   const list = (anchor) => (
-    <Box
-      sx={{ width: anchor === 'top' || anchor === 'bottom' ? 'auto' : 250 }}
+    <Box  
+      sx={{ width: anchor === "top" || anchor === "bottom" ? "auto" : 250, overflowX:"hidden" }}
       role="presentation"
       onClick={toggleDrawer(anchor, false)}
       onKeyDown={toggleDrawer(anchor, false)}
     >
       <List>
-        <ListItem key="Home">
+        <ListItem key="SidebarHeader" padding="0px">
+          <IconButton aria-label="menu" onClick={toggleDrawer("left", true)}>
+            <MenuIcon />
+          </IconButton>
+          <div id="logoSidebar">
+            <Link to="/" rel="noreferrer" className="home">
+              <h1
+                className="logo"
+                style={{
+                  margin: "10px",
+                  marginTop: "0px",
+                  marginLeft: "15px",
+                  color: "white",
+                  paddingTop: "2px",
+                  paddingBottom: "2px",
+                }}
+              >
+                Youtube
+              </h1>
+            </Link>
+          </div>
+        </ListItem>
+        <ListItem key="Home" style={{ maxHeight: "50px" }}>
           <ListItemButton>
-            <HomeIcon/>
-            <ListItemText primary={"Home"}/>  
+            <ListItemIcon>
+              <HomeIcon />
+            </ListItemIcon>
+            <ListItemText primary={"Home"} />
+          </ListItemButton>
+        </ListItem>
+        <ListItem key="Shorts" style={{ maxHeight: "50px" }}>
+          <ListItemButton>
+            <ListItemIcon>
+              <SlideshowIcon />
+            </ListItemIcon>
+            <ListItemText primary={"Shorts"} />
+          </ListItemButton>
+        </ListItem>
+        <ListItem key="Subscriptions" style={{ maxHeight: "50px" }}>
+          <ListItemButton>
+            <ListItemIcon>
+              <SubscriptionsIcon />
+            </ListItemIcon>
+            <ListItemText primary={"Subscriptions"} />
           </ListItemButton>
         </ListItem>
       </List>
-      <Divider />
+      <Divider />  
       <List>
-        
+        <ListItem key="You" style={{ maxHeight: "50px" }}>
+          <ListItemButton>
+            <ListItemIcon>
+              <VideoLibraryIcon/>
+            </ListItemIcon>
+            <ListItemText primary={"You"}/>
+          </ListItemButton>
+        </ListItem>
+        <ListItem key="History" style={{ maxHeight: "50px" }}>
+          <ListItemButton>
+            <ListItemIcon>
+              <HistoryIcon/>
+            </ListItemIcon>
+            <ListItemText primary={"History"}/>
+          </ListItemButton>
+        </ListItem>
+      </List>
+      <Divider />  
+      <List>
+        <ListItem >
+          <ListItemText primary={"Sign in to like videos, comment, and subscribe."}/>
+        </ListItem>
+        <ListItem style={{ dispaly:"flex", justifyContent:"center"}}>
+          <Button
+            variant="outlined"
+            style={{
+              marginRight: "15px",
+              textTransform: "none",
+              borderRadius: "32px",
+            }}>
+            <PersonOutlineIcon />
+            Sign in
+          </Button>
+        </ListItem>
+      </List>
+      <Divider />  
+      <List>
+        <ListItem>
+          <ListItemButton>
+            <ListItemIcon>
+              <SettingsIcon/>
+            </ListItemIcon>
+            <ListItemText primary={"Settings"}/>
+          </ListItemButton>
+        </ListItem>
+      </List>
+      <Divider />  
+      <List>
+        <ListItem>
+          <ListItemText secondary={"Created for the purpose of demostrating React skills"}/>
+        </ListItem>
       </List>
     </Box>
   );
 
   return (
-    <ThemeProvider theme={darkTheme}>
-      <CssBaseline />
-      <div className="header">
-        <div id="sidebarIcon" style={{ flex:"1 0 1%", marginLeft:"5px"}}>
-          <React.Fragment key={'left'}>
-            <IconButton aria-label="menu" onClick={toggleDrawer('left', true)}><MenuIcon/></IconButton>
-            <Drawer
-              anchor={'left'}
-              open={state['left']}
-              onClose={toggleDrawer('left', false)}
-            >
-              {list('left')}
-            </Drawer>
-          </React.Fragment>
-        </div>
-        <div id="logo" style={{ flex:"1 0 5%" }}>
-          <Link to="/" rel="noreferrer" className="home">
-            <h1 className="logo" style={{color:"white", paddingTop:"2px", paddingBottom:"2px"}}>Youtube</h1>
-          </Link>
-        </div>
-        <div id="searchBar" style={{ flex:"1 0 75%", display:"flex", justifyContent:"center" }}>
-          <Searchbar/>
-          <IconButton type="button" sx={{ p: '10px' }} aria-label="mic" style={{ marginLeft:"5px" }}>
-            <MicIcon/>
+    <div className="header" display="flex">
+      <div id="sidebarIcon" style={{  marginLeft: "15px", }}>
+        <React.Fragment key={"left"}>
+          <IconButton aria-label="menu" onClick={toggleDrawer("left", true)}>
+            <MenuIcon />
           </IconButton>
-        </div>
-        <div id="profile" style={{ flex:"1 0 15%"}}>
-          <IconButton type="button" sx={{ p: '10px' }} aria-label="settings">
-            <MoreVertIcon/>
-          </IconButton>
-          <Button variant="outlined" style={{ textTransform:"none", borderRadius:"32px"}}><PersonOutlineIcon/>Sign in</Button>
-        </div> 
-        
+          <Drawer
+            id="collapsing"
+            anchor={"left"}
+            open={state["left"]}
+            onClose={toggleDrawer("left", false)}
+          >
+            {list("left")}
+          </Drawer>
+        </React.Fragment>
       </div>
-    </ThemeProvider>
+      <div id="logo" style={{  width: "200px", maxWidth:"200px"}}>
+        <Link to="/" rel="noreferrer" className="home">
+          <h1
+            className="logo"
+            style={{ color: "white", paddingTop: "2px", paddingBottom: "2px" , width:"120px"}}
+          >
+            Youtube
+          </h1>
+        </Link>
+      </div>
+      <div
+        id="searchBar"
+        style={{ flex: "1 0 70%", display: "flex", justifyContent: "center" }}
+      >
+        <Searchbar />
+        <IconButton
+          type="button"
+          sx={{ p: "10px" }}
+          aria-label="mic"
+          style={{ marginLeft: "5px" }}
+        >
+          <MicIcon />
+        </IconButton>
+      </div>
+      <div
+        id="profile"
+        style={{ display: "flex", justifyContent: "flex-end" }}
+      >
+        <IconButton type="button" sx={{ p: "10px" }} aria-label="settings">
+          <MoreVertIcon />
+        </IconButton>
+        <Button
+          variant="outlined"
+          style={{
+            marginRight: "15px",
+            textTransform: "none",
+            borderRadius: "32px",
+          }}
+        >
+          <PersonOutlineIcon />
+          Sign in
+        </Button>
+      </div>
+    </div>
   );
 }
--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -1,65 +1,47 @@
 import { Link } from "react-router-dom";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
 import React from "react";
-import logo from '../iconMenu.png';
 import thumbnail from "../videoPlaceholder.jpg";
-import  Header from "./header.jsx";
-import '../App.css';
+import Header from "./header.jsx";
+import "../App.css";
 
+const darkTheme = createTheme({
+  palette: {
+    mode: "dark",
+  },
+});
 
-
-function SidebarMenu({clickFunction}){  
-  return(
-     <img className="hambMenu" src={logo} alt="menu" width="40" height="40" onClick={()=>clickFunction()}/>
-    );
-  }
-
-function VideoItem({video}){
-
-  return(
+function VideoItem({ video }) {
+  return (
     <Link to={`/video/${video.name}`} rel="noreferrer" className="home">
       <div className="videoItem">
-          <img src={thumbnail} alt="video img" width="250" height="150"/>
+        <img src={thumbnail} alt="video img" width="250" height="150" />
         <div className="videoDescription">
-          <img className="profile" src={thumbnail} alt="video img" width="40" height="40" />
+          <img
+            className="profile"
+            src={thumbnail}
+            alt="video img"
+            width="40"
+            height="40"
+          />
           <p className="videoTitle">{video.name}</p>
         </div>
       </div>
     </Link>
   );
 }
-  
-  function VideoGrid({ videos }){
-    const videoList = [];
-  
-    videos.forEach((video) =>{
-      videoList.push(video);
-    });
-    return(
-      <>
-        {videoList.map(video => (
-          <VideoItem key={video.name} video={video}/>
-        ))}
-      </>
-    );
-  }
 
-export default function Root(){  
-    return (
-      <div id="Outer">
-        <Header/>
+export default function Root() {
+  return (
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
+      <div id="Outer" style={{ display: "flex" }}>
+        <div id="Header">
+          <Header />
+        </div>
+        <div id="Body"></div>
       </div>
-    );
-  }
-const VIDEOS = [
-  {name:"Video 1", category:"Test"},
-  {name:"Video 2", category:"Test1"},
-  {name:"Video 3", category:"Test"},
-  {name:"Video 4", category:"Test"},
-  {name:"Video 5", category:"Test1"},
-  {name:"Video 6", category:"Test1"},
-  {name:"Video 7", category:"Test1"},
-  {name:"Video 8", category:"Test1"},
-  {name:"Video 9", category:"Test1"},
-  {name:"Video 10", category:"Test1"},
-  {name:"Video 11", category:"Test1"}
-];
+    </ThemeProvider>
+  );
+}
